1cc094b2200238a96e9a8679181a4fad3fd2e0d6
vishalmunic17 <vqm5223@psu.edu>
2020-09-03 14:47:51 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+// Author: Vishal Munic vqm5223@psu.edu
+// Collaborator: Abishek  apv5314@psu.edu
+// Collaborator: JIULONG TANG jzt5526@psu.edu
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+
+int main(void) {
+  char *tempstr = readline("Enter temperature in Celsius: ");
+  double temp = atof(tempstr);
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", temp, (temp * 1.8)+32);
+  return 0;
+}
